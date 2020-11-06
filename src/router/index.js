53c8a26f3d427e554dcaeacb54eaeeb3452cf41d
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-11-06 10:08:17 +0100

    Configuring router

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,14 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
-
+const routes = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }
+]
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: routes,
+  mode: 'history'
 })
