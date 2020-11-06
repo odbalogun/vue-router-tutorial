f2af054e8a4529a7fe39c9aad337ef3706ae0296
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-11-06 15:13:48 +0100

    Added a template to Param.vue

--- src/components/Param.vue
+++ src/components/Param.vue
@@ -1,3 +1,10 @@
+<template>
+  <div class="param">
+    <input type="text" size="30" v-model="UserInput" />
+    <input type="button" value="Go to route" @click="GoToRoute()">
+  </div>
+</template>
+
 <script>
 export default {
   name: 'Param',
